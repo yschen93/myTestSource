3040ca32ee363f4312ec497b95617750a1e11218
yschen93 <xz_yschen@163.com>
2022-08-16 15:00:40 +0800

    commit boost_unit_test_framework

--- try_catch.cpp
+++ try_catch.cpp
@@ -4,9 +4,23 @@
 #include <iostream>
 using namespace std;
 
+class SelfManager
+{
+public:
+    SelfManager()
+    {
+        std::cout << "SelfManager()" << std::endl;
+    }
+    ~SelfManager()
+    {
+        std::cout << "~SelfManager()" << std::endl;
+    }
+};
+
 int main(int argc, char *argv[])
 {
     int a, b = 0;
+    SelfManager();
     try{
         if (b == 0)
             throw b;
